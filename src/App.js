7e34d60a2b259f5dd07c868f62560e270e408b66
tjs6595 <t.schultejans@schenckprocess.com>
2022-07-08 19:47:13 -0500

    Initial Commit from GitBash.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// Import data
 
-function App() {
+// Import components
+import './App.css';
+import {useState, useEffect} from 'react';
+import Gallery from './Gallery';
+import ButtonsBar from './ButtonsBar';
+
+
+function App(){
+  let [score, setScore] = useState(0)
+  let [data, setData] = useState({})
+  const handleIterate = (e) => {
+    setArtId(artId + Number(e.target.value))
+  }
+  useEffect(() => {
+    document.title='Welcome to ArtWorld!'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+  }, [artId])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Art Gallery</h1>
+      <div className="App">
+        <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+        <ButtonsBar handleIterate = {handleIterate}/>
+      </div>
     </div>
   );
 }
